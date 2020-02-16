26b7870695c8e02bcd07ca63b21180cccb031a27
wjsxh <525583382@qq.com>
2020-02-16 10:43:21 +0800

    Update git_study.py

--- git_study.py
+++ git_study.py
@@ -1,1 +1,18 @@
-print("Hello World")
+
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print('call %s():' % func.__name__)
+        print('args = {}'.format(*args))
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@log
+def fuck(num:int):
+    print(num)
+
+fuck(3)
